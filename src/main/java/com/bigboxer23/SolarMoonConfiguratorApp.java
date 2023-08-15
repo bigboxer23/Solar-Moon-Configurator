16e045e8fed58785c70e98245f57a140b9ce5ae4
Matt Jones <bigboxer23@gmail.com>
2023-08-14 20:49:19 -0500

    skeleton of working cognito integration (sans application.yml)

---
+++ src/main/java/com/bigboxer23/SolarMoonConfiguratorApp.java
@@ -0,0 +1,12 @@
+package com.bigboxer23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** */
+@SpringBootApplication
+public class SolarMoonConfiguratorApp {
+	public static void main(String[] args) {
+		SpringApplication.run(SolarMoonConfiguratorApp.class, args);
+	}
+}
