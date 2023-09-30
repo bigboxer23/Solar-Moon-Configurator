2448f0362dfa714201cb5a689fc71784999e1fa4
Matt Jones <bigboxer23@gmail.com>
2023-09-30 16:52:44 -0500

    moving some methods to common for use in lambda (#15)

--- src/main/java/com/bigboxer23/solar_moon/ExtendedCustomerComponent.java
+++ src/main/java/com/bigboxer23/solar_moon/ExtendedCustomerComponent.java
@@ -17,21 +17,6 @@ public class ExtendedCustomerComponent extends CustomerComponent {
 				Arrays.asList(Customer.ACCESS_KEY_INDEX, Customer.CUSTOMER_ID_INDEX), getTable());
 	}
 
-	public Customer addCustomer(String email, String customerId) {
-		if (email == null || customerId == null || email.isBlank() || customerId.isBlank()) {
-			logger.warn("email or customer id is null/empty, cannot fetch.");
-			return null;
-		}
-		if (findCustomerByCustomerId(customerId) != null) {
-			logger.debug(customerId + ":" + email + " exists, not putting into db");
-			return null;
-		}
-		Customer customer = new Customer(customerId, email, TokenGenerator.generateNewToken());
-		logAction(customer.getCustomerId(), "add");
-		getTable().putItem(customer);
-		return customer;
-	}
-
 	public void updateCustomer(Customer customer) {
 		if (customer == null || customer.getCustomerId() == null || customer.getEmail() == null) {
 			logger.warn("invalid customer passed, not updating");
@@ -53,18 +38,6 @@ public class ExtendedCustomerComponent extends CustomerComponent {
 			logAction(customer.getCustomerId(), "delete by customer id");
 			getTable().deleteItem(customer);
 		});
-	}
-
-	public Customer findCustomerByCustomerId(String customerId) {
-		return customerId != null && !customerId.isEmpty()
-				? this.getTable()
-						.index(Customer.CUSTOMER_ID_INDEX)
-						.query(QueryConditional.keyEqualTo((builder) -> builder.partitionValue(customerId)))
-						.stream()
-						.findFirst()
-						.flatMap((page) -> page.items().stream().findFirst())
-						.orElse(null)
-				: null;
 	}
 
 	private void logAction(String action, String customerId) {
