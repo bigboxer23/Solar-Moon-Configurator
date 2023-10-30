d081a68b0f51170579d5059c1c1f6046e0b0ebf6
Matt Jones <bigboxer23@gmail.com>
2023-10-30 10:59:45 -0500

    add table creation code for subscriptions (#20)

---
+++ src/main/java/com/bigboxer23/solar_moon/ExtendedSubscriptionComponent.java
@@ -0,0 +1,13 @@
+package com.bigboxer23.solar_moon;
+
+import com.bigboxer23.solar_moon.util.TableCreationUtils;
+import java.util.Collections;
+import org.springframework.stereotype.Component;
+
+/** */
+@Component
+public class ExtendedSubscriptionComponent extends SubscriptionComponent {
+	public void createSubscriptionTable() {
+		TableCreationUtils.createTable(Collections.emptyList(), getTable());
+	}
+}
