e21e2fd37fd7cf92a0f0429cca11a701815a8055
Matt Jones <bigboxer23@gmail.com>
2023-11-12 21:44:30 -0600

    new index to table bump lib version

--- src/main/java/com/bigboxer23/solar_moon/alarm/ExtendedAlarmComponent.java
+++ src/main/java/com/bigboxer23/solar_moon/alarm/ExtendedAlarmComponent.java
@@ -1,6 +1,7 @@
 package com.bigboxer23.solar_moon.alarm;
 
 import com.bigboxer23.solar_moon.AlarmComponent;
+import com.bigboxer23.solar_moon.DeviceComponent;
 import com.bigboxer23.solar_moon.OpenWeatherComponent;
 import com.bigboxer23.solar_moon.data.Alarm;
 import com.bigboxer23.solar_moon.util.TableCreationUtils;
@@ -11,18 +12,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class ExtendedAlarmComponent extends AlarmComponent {
 
-	public ExtendedAlarmComponent(OpenWeatherComponent openWeatherComponent) {
-		super(openWeatherComponent);
+	public ExtendedAlarmComponent(OpenWeatherComponent openWeatherComponent, DeviceComponent deviceComponent) {
+		super(openWeatherComponent, deviceComponent);
 	}
 
 	public void createAlarmTable() {
 		TableCreationUtils.createTable(
 				Arrays.asList(
+						Alarm.CUSTOMER_INDEX,
 						Alarm.SITE_CUSTOMER_INDEX,
 						Alarm.SITE_STATE_INDEX,
 						Alarm.DEVICE_STATE_INDEX,
 						Alarm.STATE_CUSTOMER_INDEX,
-						Alarm.DEVICE_CUSTOMER_INDEX),
+						Alarm.DEVICE_CUSTOMER_INDEX,
+						Alarm.DEVICEID_STARTDATE_INDEX),
 				getTable());
 	}
 }
