4246e3ab03cff043af9418c0d97bfc340828cb48
Matt Jones <bigboxer23@gmail.com>
2023-11-21 19:23:03 -0600

    add maintenance table

---
+++ src/main/java/com/bigboxer23/solar_moon/notification/ExtendedNotificationComponent.java
@@ -0,0 +1,8 @@
+package com.bigboxer23.solar_moon.notification;
+
+import com.bigboxer23.solar_moon.notifications.NotificationComponent;
+import org.springframework.stereotype.Component;
+
+/** */
+@Component
+public class ExtendedNotificationComponent extends NotificationComponent {}
