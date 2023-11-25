29179037afd8016666d94c0fab372898f9a5b709
Matt Jones <bigboxer23@gmail.com>
2023-11-25 11:21:58 -0600

    adding device update table

---
+++ src/test/java/com/bigboxer23/solar_moon/TestExtendedDeviceUpdateComponent.java
@@ -0,0 +1,23 @@
+package com.bigboxer23.solar_moon;
+
+import com.bigboxer23.solar_moon.device.ExtendedDeviceUpdateComponent;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+/**
+ *
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class TestExtendedDeviceUpdateComponent
+{
+	@Autowired
+	private ExtendedDeviceUpdateComponent component;
+
+	@Test
+	public void createDeviceUpdateTable() {
+		component.createDeviceUpdateTable();
+	}
+}
