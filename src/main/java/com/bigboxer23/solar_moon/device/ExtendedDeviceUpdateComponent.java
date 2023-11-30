48002766cc85378e459c056f7990b3e5d0c59d47
Matt Jones <bigboxer23@gmail.com>
2023-11-29 21:39:47 -0600

    update for j21

--- src/main/java/com/bigboxer23/solar_moon/device/ExtendedDeviceUpdateComponent.java
+++ src/main/java/com/bigboxer23/solar_moon/device/ExtendedDeviceUpdateComponent.java
@@ -3,16 +3,12 @@ package com.bigboxer23.solar_moon.device;
 import com.bigboxer23.solar_moon.DeviceUpdateComponent;
 import com.bigboxer23.solar_moon.data.DeviceUpdateData;
 import com.bigboxer23.solar_moon.util.TableCreationUtils;
+import java.util.Collections;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
-
-/**
- *
- */
+/** */
 @Component
-public class ExtendedDeviceUpdateComponent extends DeviceUpdateComponent
-{
+public class ExtendedDeviceUpdateComponent extends DeviceUpdateComponent {
 	public void createDeviceUpdateTable() {
 		TableCreationUtils.createTable(Collections.singletonList(DeviceUpdateData.IDENTITY_UPDATE_INDEX), getTable());
 	}
