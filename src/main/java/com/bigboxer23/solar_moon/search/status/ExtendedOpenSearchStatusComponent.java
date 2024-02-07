1c3e6765aefc6af138943382ae58e166c2a970bf
Matt Jones <bigboxer23@gmail.com>
2024-02-07 11:01:40 -0600

    feat: add creation for download request table (#32)

--- src/main/java/com/bigboxer23/solar_moon/search/status/ExtendedOpenSearchStatusComponent.java
+++ src/main/java/com/bigboxer23/solar_moon/search/status/ExtendedOpenSearchStatusComponent.java
@@ -1,14 +1,10 @@
 package com.bigboxer23.solar_moon.search.status;
 
 import com.bigboxer23.solar_moon.util.TableCreationUtils;
-
 import java.util.Collections;
 
-/**
- *
- */
-public class ExtendedOpenSearchStatusComponent extends OpenSearchStatusComponent
-{
+/** */
+public class ExtendedOpenSearchStatusComponent extends OpenSearchStatusComponent {
 
 	public void createOpenSearchStatusTable() {
 		TableCreationUtils.createTable(Collections.emptyList(), getTable());
